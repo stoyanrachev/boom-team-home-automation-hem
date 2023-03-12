8818127525743cfb65709a4fe53c03b6eb26d3f2
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-03-12 15:56:28 +0200

    Login Page try to fix 1

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,18 +1,24 @@
 import Login from "../../src/components/login/Login";
 import classNames from "classnames";
-import { Container, Paper } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
 import styles from "./LoginPage.module.scss";
 import Link from "../../src/components/link/Link";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
-      <Paper className={classNames(styles["paper"])}>
-        <Container className={classNames(styles["container"])}>
+      <Container className={classNames(styles["container"])}>
+        <Paper className={classNames(styles["paper"])}>
           <Login />
-        </Container>
-      </Paper>
-      <p>Don't have account yet? <Link href="/register" className="link-to">Go to register.</Link></p>
+        </Paper>
+      </Container>
+      <p>
+        Don't have account yet?{" "}
+        <Link href="/register" className="link-to">
+          Go to register.
+        </Link>
+      </p>
     </div>
   );
 }
