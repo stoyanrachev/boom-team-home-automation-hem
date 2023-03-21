42d505f06a1f5b25eee93aa033cc764e292e0b91
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-03-21 21:22:02 +0200

    Navigation init

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,98 @@
+import styles from "./Navigation.module.scss";
+import classNames from "classnames";
+import { useState } from "react";
+import {
+  Drawer,
+  List,
+  IconButton,
+  Menu,
+  ListItem,
+  MenuItem,
+  ListItemIcon,
+  Badge,
+  ListItemText,
+} from "@mui/material";
+import User from "../user/User";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BedOutlinedIcon from "@mui/icons-material/BedOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Link from "../link/Link";
+
+export default function Navigation() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classNames(styles["navigation"])}>
+      <Drawer anchor="left" variant="permanent">
+        <div className={classNames(styles["user-container"])}>
+          <User
+            name="John Doe"
+            avatar="/images/avatar.png"
+            size={55}
+            headingSize="h5"
+          />
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? "long-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                width: "144px",
+              },
+            }}
+          >
+            <MenuItem>Logout</MenuItem>
+          </Menu>
+        </div>
+
+        <List>
+          {/* Main Nav link */}
+          <Link href="#">
+            <ListItem className={classNames(styles["header-item"])}>
+              <ListItemIcon>
+                <HomeOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Apartment" />
+              <Badge
+                badgeContent={5}
+                color="secondary"
+                className={classNames(styles["badge"])}
+              />
+            </ListItem>
+          </Link>
+          {/* Sub Nav link */}
+          <Link href="#">
+            <ListItem>
+              <ListItemIcon>
+                <BedOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Living Room" />
+              <Badge
+                badgeContent={"!"}
+                color="error"
+                className={classNames(styles["badge"])}
+              />
+            </ListItem>
+          </Link>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Container, Grid, Avatar, Typography } from '@mui/material';
+import React from "react";
+import { Container, Grid, Avatar, Typography } from "@mui/material";
 import styles from "./User.module.scss";
 
-
-export default function User({avatar, name, size }) {
-    return (
-        <div className={styles['user-container']}>
-            <Container >
-                <Grid container spacing={2}  justifyContent="center">
-                    <Grid item xs={1} className={styles['grid-avatar']}>
-                        <Avatar alt={name} src={avatar} sx={{ width: size, height: size }} />
-                    </Grid>
-                    <Grid item xs={11} className={styles['grid-text']}>
-                        <Typography variant="h1">
-                            {name}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>
-    )
+export default function User({
+  avatar,
+  name,
+  size,
+  headingSize = "h1",
+  hasWelcome = false,
+}) {
+  return (
+    <div className={styles["user-container"]}>
+      <Container>
+        <Grid container spacing={1} alignItems="center" justifyContent="center">
+          <Grid item xs={4} className={styles["grid-avatar"]}>
+            <Avatar
+              alt={name}
+              src={avatar}
+              sx={{ width: size, height: size }}
+            />
+          </Grid>
+          <Grid item xs={8} className={styles["grid-text"]}>
+            {hasWelcome && (
+              <Typography className={styles.welcome}>Welcome back,</Typography>
+            )}
+            <Typography variant={headingSize}>{name}</Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
-
-
-
-
-
