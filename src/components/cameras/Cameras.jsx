d4351076613445f3895c59f812243406c9298929
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-23 19:18:32 +0200

    Cameras Component

--- src/components/cameras/Cameras.jsx
+++ src/components/cameras/Cameras.jsx
@@ -1,15 +1,63 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Cameras.module.scss";
 import MUICard from '@mui/material/Card';
-import { CardContent } from "@mui/material";
+import {Container, Grid } from "@mui/material";
+import Card from "../card/Card";
 
 
-export default function Cameras ({ iconUrl, outlined = false, onClick, mediaType }) {
-    
-   
+export default function Cameras({
+    cameras = [
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"  },
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" },
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" },
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" },
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" },
+        { videoUrl: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" },
+    ],
+    hasButton = true
+}) {
+
+
+    const [selectedCard, setSelectedCard] = useState(0);
+
+    const handleCardClick = (index) => {
+        setSelectedCard(index);
+    };
+
     return (
-        <div className={styles.cameras}>
-          
+        <div className={styles['cameras-wrapper']}>
+            <Grid container spacing={1} style={{ width: '100%', height: '100%' }}>
+                <Grid item xs={7} style={{ width: '100%', height: '100%' }}>
+                    <Card iconUrl={cameras[selectedCard].videoUrl} mediaType='video' autoPlay={true} />
+                </Grid>
+
+                <Grid item xs={5}  >
+                    <Container disableGutters style={{ width: '100%', height: '100%' }}>
+                        <Grid container spacing={1}
+                            className={`${styles.cards} ${hasButton ? styles.hasButton : ""}`}
+                        >
+
+                            {cameras.map((camera, index) => (
+
+                                <Grid item key={index} xs={6} className={styles.card} onClick={() => handleCardClick(index)}>
+
+                                    <Card iconUrl={camera.videoUrl} outlined={selectedCard === index} mediaType='video'/>
+
+                                </Grid>
+
+                            ))}
+                        </Grid>
+                        {hasButton ? (
+                            <Grid container spacing={1} >
+                                <Grid item xs={12} className={styles.plus} >
+                                    <Card iconUrl={"/images/plus.svg"} outlined={true} />
+                                </Grid>
+                            </Grid>
+                        ) : null}
+                    </Container>
+                </Grid>
+
+            </Grid>
         </div>
     );
-};
+}
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Card.module.scss";
 import MUICard from '@mui/material/Card';
-import { CardContent } from "@mui/material";
+import { CardContent, CardMedia } from "@mui/material";
 
 
-export default function Card({ iconUrl, outlined = false, onClick, mediaType }) {
-    
+export default function Card({ iconUrl, outlined = false, onClick, mediaType,autoPlay }) {
+
     const handleClick = () => {
         if (onClick) {
-          onClick();
+            onClick();
         }
-      };
+    };
 
     return (
         <div className={styles.card} onClick={handleClick}>
-            <MUICard className={`${styles.content} ${outlined ? styles.outlined : ""}`}>
-                <CardContent>
-                    {iconUrl && <img src={iconUrl} alt="icon" />}
+            <MUICard className={`${styles.content} ${outlined ? styles.outlined : ""}`} >
+                <CardContent   >
+                    {mediaType === "video" ? (
+                        <CardMedia
+                            component='video'
+                            image={iconUrl}
+                            autoPlay={autoPlay}
+                        />
+                    ) : (
+                        iconUrl && <img src={iconUrl} alt="icon" className={styles.image} />
+                    )}
                 </CardContent>
             </MUICard>
         </div>
