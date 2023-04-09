3bb8dac620ba1cd2ed665b9cbc5425a3f0eaa5c0
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-04-09 18:04:35 +0300

    Implement Login API Calls

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -4,13 +4,36 @@ import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import styles from "./LoginPage.module.scss";
 import Link from "../../src/components/link/Link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  const [errors, setErrors] = useState({});
+
+  async function onSubmit(userData) {
+    const result = await fetch("https://project-5-api.boom.dev/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const data = await result.json();
+    if (result.status !== 200 && result.status !== 201) {
+      setErrors(data);
+    } else {
+      localStorage.setItem("accessToken", data.accessToken);
+      router.push("/");
+    }
+  }
+
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles["container"])}>
         <Paper className={classNames(styles["paper"])}>
-          <Login />
+          <Login onSubmit={onSubmit} err={errors} />
         </Paper>
       </Container>
       <p>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,35 @@
 import styles from "./Login.module.scss";
 import classNames from "classnames";
-import { Button, InputAdornment, TextField } from "@mui/material";
+import {
+  Alert,
+  Button,
+  InputAdornment,
+  Snackbar,
+  TextField,
+} from "@mui/material";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
+import { useEffect, useState } from "react";
 
-export default function Login() {
+export default function Login({ onSubmit, err }) {
+  const [userData, setUserData] = useState({});
+  const [errorState, setErrorState] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    if (typeof err == "string") {
+      setErrorMessage(err);
+      setErrorState(true);
+    }
+  }, [err, userData]);
+
+  function handleClose(event, reason) {
+    if (reason === "clickaway") {
+      return;
+    }
+    setErrorState(false);
+  }
+
   return (
     <div className={classNames(styles["login-form"])}>
       <TextField
@@ -12,6 +37,11 @@ export default function Login() {
         type="email"
         variant="standard"
         placeholder="Email"
+        onChange={(ev) =>
+          setUserData(
+            Object.assign(userData, { email: ev.target.value.trim() })
+          )
+        }
         className={classNames(styles["input-field"])}
         InputProps={{
           startAdornment: (
@@ -27,6 +57,11 @@ export default function Login() {
         variant="standard"
         placeholder="Password"
         id="pass"
+        onChange={(ev) =>
+          setUserData(
+            Object.assign(userData, { password: ev.target.value.trim() })
+          )
+        }
         className={classNames(styles["input-field"])}
         InputProps={{
           startAdornment: (
@@ -40,8 +75,21 @@ export default function Login() {
         fullWidth
         variant="contained"
         className={classNames(styles["button"])}
+        onClick={() => {
+          document.getElementById("pass").value = "";
+          onSubmit(userData);
+        }}
       >
         Login
+        <Snackbar
+          open={errorState}
+          autoHideDuration={5000}
+          onClose={handleClose}
+        >
+          <Alert severity="error" onClose={handleClose}>
+            {errorMessage}
+          </Alert>
+        </Snackbar>
       </Button>
     </div>
   );
